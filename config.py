1229bd6141de3641907eb0f10072601b9afd797c
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-05-05 20:15:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29707621
-api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
-bot_token = "6634971116:AAGhU6IQBREjXdliqJr9CGh1ZCLpoYbGP5o"
-auth_users = "6707073614, 1406300051, 6801378994, 870101201"
+api_id = "29938092"
+api_hash = "1b4a57d7397be415d8d4e7182d44d95e"
+bot_token = "6501001560:AAFZoKieXg_RRZnuU9rkPzr9f5TvP688JCg"
+auth_users = "1280494242, 1406300051"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6707073614"
+osowner_users = "1280494242"
 owner_users = [int(num) for num in osowner_users.split(",")]
