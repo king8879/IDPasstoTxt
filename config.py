b72e3d212192d195dfe320dad618d55d750fe3f7
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-05-10 12:00:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = "29938092"
-api_hash = "1b4a57d7397be415d8d4e7182d44d95e"
-bot_token = "6501001560:AAFZoKieXg_RRZnuU9rkPzr9f5TvP688JCg"
-auth_users = "1280494242, 1406300051"
+api_id = "29251040"
+api_hash = "a1de34e4ad21cc9fb34798473eaf7bab"
+bot_token = "AAHM8aqhPNbxiVa_JMZ0msApxuQOX1IpG24"
+auth_users = "1280494242, 1406300051, 6539854720"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "1280494242"
 owner_users = [int(num) for num in osowner_users.split(",")]
