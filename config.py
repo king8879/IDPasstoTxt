9acca29ab5b3b2165d951009cd57e63fd12423a7
Unique <mujeem000@gmail.com>
2024-06-27 16:24:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = "21179966"
 api_hash = "d97919fb0a3c725e8bb2a25bbb37d57c"
-bot_token = "7099851813:AAEORRZoupPNi5cugUmH4hXqhvnYjEcFhFc"
+bot_token = "7332862690:AAGQhPugLb6ItEaxi2EuQRekUYmdnwig2c4"
 auth_users = "7326397503"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "7326397503"
