cd32a7e75335179500eaaabc40319496393b31f7
king8879 <k4507244@gmail.com>
2025-04-24 22:09:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = "29251040"
-api_hash = "a1de34e4ad21cc9fb34798473eaf7bab"
-bot_token = "AAHM8aqhPNbxiVa_JMZ0msApxuQOX1IpG24"
-auth_users = "1280494242, 1406300051, 6539854720"
+api_id = "22528446"
+api_hash = "0d81bf18019c5f3839037d0ae737c358"
+bot_token = "7580026996:AAGA6SYg9Zl6NMSnwbYxSAtKMTtEfCTSXz0"
+auth_users = "633111330"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "1280494242"
+osowner_users = "633111330"
 owner_users = [int(num) for num in osowner_users.split(",")]
