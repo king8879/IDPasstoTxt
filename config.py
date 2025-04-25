e92b4852e0e7212b63e88899915be981b795e85d
king8879 <k4507244@gmail.com>
2025-04-25 19:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = "22528446"
-api_hash = "0d81bf18019c5f3839037d0ae737c358"
-bot_token = "7580026996:AAGA6SYg9Zl6NMSnwbYxSAtKMTtEfCTSXz0"
-auth_users = "633111330"
+api_id = "18579024"
+api_hash = "124981da628d86e21ee492da77cd4037"
+bot_token = "7994904406:AAEmKvtbSe4b3UINDxWQqqnq3GE-puYpuv8"
+auth_users = "574224129"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "633111330"
+osowner_users = "574224129"
 owner_users = [int(num) for num in osowner_users.split(",")]
